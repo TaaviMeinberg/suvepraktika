90b91a678b501217a6efcc773c6dfa22fe98869f
mhaava <mihkel9999@hotmail.com>
2018-06-08 14:40:45 +0300

    test

--- php/form_submit/tudengiprojekti_aruandlus_submit.php
+++ php/form_submit/tudengiprojekti_aruandlus_submit.php
@@ -1,9 +1,45 @@
 <?php
+
+$name;
+$id;
+$phone;
+$email;
+$aadress;
+$bank_acc;
+$report_compiler;
+$project_manager;
+$team_members;
+$supervisor_name;
+$supervisor_occupation;
+$field_of_activity;
+$project_name;
+$initial_date;
+$end_date;
+$grant_awarded;
+$actual_cost;
+$problem;
+$project_goal;
+$expected_results;
+$actual_results;
+$planned_activities;
+$planned_m1;
+$actual_m1;
+$planned_m2;
+$actual_m2;
+$planned_m3;
+$actual_m3;
+$additional_info;
+
+//table
+
+
+
+
 function submitForm(){
 
-  $mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+  //$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
 
-  //$stmt = $mysqli->prepare("INSERT INTO vp1users (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+  $stmt = $mysqli->prepare("INSERT INTO vp1users (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
   echo $mysqli->error;
 
   //$stmt->bind_param("sssiss", $signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword);

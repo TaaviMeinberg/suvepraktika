8a75ed9a31a7ea18ffafc548400236434497449d
mhaava <mhaava@tlu.ee>
2018-06-12 11:09:25 +0300

    vaja lisada veel tabeli lahtrite nimed päringusse

--- php/form_submit/teadusprojekti_aruandlus_submit.php
+++ php/form_submit/teadusprojekti_aruandlus_submit.php
@@ -82,10 +82,10 @@ function submitForm(){
 
   $mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
 
-  $stmt = $mysqli->prepare("INSERT INTO vp1users (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+  $stmt = $mysqli->prepare("INSERT INTO scientific_project_report (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
   echo $mysqli->error;
 
-  $stmt->bind_param("sssssssssssssssiisssssssssssssiis", $name, $id, $phone, $email, $address, $bank_acc, $report_compiler, $project_manager, $team_members, $supervisor_name, $supervisor_occupation, $field_of_activity, $project_name, $initial_date, $end_date, $grant_awarded, $actual_cost, $problem, $project_goal, $expected_results, $actual_results, $planned_activities, $planned_m1, $actual_m1, $planned_m2, $actual_m2, $planned_m3, $actual_m3, $additional_info, $jsonTable, $project_budget_total, $requested_budget, $budget_explanation);
+  $stmt->bind_param("sssssssssssssssddsssssssssssssdds", $name, $id, $phone, $email, $address, $bank_acc, $report_compiler, $project_manager, $team_members, $supervisor_name, $supervisor_occupation, $field_of_activity, $project_name, $initial_date, $end_date, $grant_awarded, $actual_cost, $problem, $project_goal, $expected_results, $actual_results, $planned_activities, $planned_m1, $actual_m1, $planned_m2, $actual_m2, $planned_m3, $actual_m3, $additional_info, $jsonTable, $project_budget_total, $requested_budget, $budget_explanation);
   if ($stmt->execute()){
     echo "\n Õnnestus!";
   } else {

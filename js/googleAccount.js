2419e73963f3bbcb03f786655ae77a7720e71e63
Taavi Meinberg <taavim@tlu.ee>
2018-06-13 10:24:33 +0300

    Non @tlu admins can now login

--- js/googleAccount.js
+++ js/googleAccount.js
@@ -1,10 +1,9 @@
-window.onload = function() {
+window.onload = function () {
     onLoad();
 }
 
 function onLoad() {
     gapi.load('auth2', function () {
-        console.log("Success");
         gapi.auth2.init();
     });
 }
@@ -12,40 +11,37 @@ function onLoad() {
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
 
-    if (profile.getEmail().includes("@tlu.ee")) {
-        var userName = profile.getName();
-        var userEmail = profile.getEmail();
-        var sessionStatus = "create";
+    /*   if (profile.getEmail().includes("@tlu.ee")) { */
+    var userName = profile.getName();
+    var userEmail = profile.getEmail();
+    var sessionStatus = "create";
 
-        $.post("./php/session.php", {userName:userName, userEmail:userEmail, sessionStatus:sessionStatus});
-        $.get("./php/db/isUserAdmin.php", function(result){
-            if(result =="true"){
-                window.location = "./admin.php";
-            }else{
-                window.location = "./main.php";
-            }
-        });
-    } else {
-        alert("Palun logi sisse kasutades enda TLÜ kontot.");
-        signOut();
-        console.log('User signed out.');
-        window.location.reload(true)
-    }
+    $.post("./php/session.php", { userName: userName, userEmail: userEmail, sessionStatus: sessionStatus });
+    $.get("./php/db/isUserAdmin.php", function (isAdmin) {
+        if (isAdmin == "true") {
+            window.location = "./admin.php";
+        } else if (profile.getEmail().includes("@tlu.ee")) {
+            window.location = "./main.php";
+        } else if (!profile.getEmail().includes("@tlu.ee") && isAdmin != "true") {
+            alert("Palun logi sisse kasutades enda TLÜ kontot.");
+            signOut();
+        }
+    });
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         var sessionStatus = "destroy";
-        $.post("./php/session.php", {sessionStatus:sessionStatus});
+        $.post("./php/session.php", { sessionStatus: sessionStatus });
         console.log('User signed out.');
         window.location = "./index.html";
     });
 }
-function isAdmin(){
-    $.get("./php/db/isUserAdmin.php", function(result){
-        if(result !="true"){
-			document.body.innerHTML = "";
+function isAdmin() {
+    $.get("./php/db/isUserAdmin.php", function (result) {
+        if (result != "true") {
+            document.body.innerHTML = "";
             alert("Teil puudub ligipääs");
             window.location = "./main.php";
         }

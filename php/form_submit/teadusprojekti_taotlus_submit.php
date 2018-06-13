defbbc7657c41b17567c3216a2fafa26e5e27140
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2018-06-13 09:53:31 +0300

    lisatud andmetabelisse saatmine

--- php/form_submit/teadusprojekti_taotlus_submit.php
+++ php/form_submit/teadusprojekti_taotlus_submit.php
@@ -29,6 +29,10 @@ $m1;
 $m2;
 $m3;
 $reason;
+$jsonTable;
+$project_budget_total;
+$requested_budget;
+$budget_explanation;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
@@ -57,11 +61,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $m2 = $_POST['m2'];
     $m3 = $_POST['m3'];
 	$reason = $_POST['reason'];
+	$jsonTable = $_POST['jsonTable'];
+    $project_budget_total = $_POST['project_budget_total'];
+    $requested_budget = $_POST['requested_budget'];
+    $budget_explanation = $_POST['budget_explanation'];
 
     $mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUserName"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
-    $stmt = $mysqli->prepare("INSERT INTO scientific_project_report (name, connection, code, phone, email, address, speciality, degree, year, project_manager, team_members, supervisor_name, supervisor_occupation, supervisor_field, project_name, requested_amount, initial_date, end_date, requested_amount_goal, problem, project_goal, results, activities, m1, m2, m3, reason) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    $stmt = $mysqli->prepare("INSERT INTO scientific_project_report (name, connection, code, phone, email, address, speciality, degree, year, project_manager, team_members, supervisor_name, supervisor_occupation, supervisor_field, project_name, requested_amount, initial_date, end_date, requested_amount_goal, problem, project_goal, results, activities, m1, m2, m3, reason, budget_table, budget_total, requested_budget, budget_explanation) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
     echo $mysqli->error;
-    $stmt->bind_param("sssssssssssssssdsssssssssss", $name, $connection, $id, $phone, $email, $address, $speciality, $degree, $year, $project_manager, $team_members, $supervisor_name, $supervisor_occupation, $field_of_activity, $project_name, $requested_amount, $initial_date, $end_date, $requested_amount_goal, $problem, $project_goal, $results, $activities, $m1, $m2, $m3, $reason);
+    $stmt->bind_param("sssssssssssssssdssssssssssssdds", $name, $connection, $id, $phone, $email, $address, $speciality, $degree, $year, $project_manager, $team_members, $supervisor_name, $supervisor_occupation, $field_of_activity, $project_name, $requested_amount, $initial_date, $end_date, $requested_amount_goal, $problem, $project_goal, $results, $activities, $m1, $m2, $m3, $reason, $jsonTable, $project_budget_total, $requested_budget, $budget_explanation);
     if ($stmt->execute()){
       echo "\n Salvestatud!";
     } else {
